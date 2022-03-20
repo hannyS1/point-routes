f4c00e691a7b79ce0ae08fdc832c8a39a9f20ec2
hannyS1 <rs.shakhov@gmail.com>
2022-03-20 13:00:02 +0700

    + gunicorn

--- routes/run.sh
+++ routes/run.sh
@@ -7,4 +7,4 @@ echo "Migrate..."
 alembic upgrade head
 
 echo "Run server..."
-uvicorn main:app --host 0.0.0.0 --port 8000
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
